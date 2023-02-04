ba043f87c09221c14a5200e6de0c1ead4db613d7
Mikael <mikaelkawam.dev@gmail.com>
2023-02-04 15:52:58 -0300

    App code

---
+++ backend/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+async function main() {
+    await mongoose.connect("mongodb://0.0.0.0:27017/getpet");
+    console.log('Conectou ao Mongoose')
+}
+
+
+main().catch((err) => console.log(err))
+
+
+module.exports = mongoose
---
+++ backend/helpers/get-token.js
@@ -0,0 +1,11 @@
+const getToken = (req) => {
+
+
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(" ")[1];
+
+
+    return token
+}  
+
+module.exports = getToken
